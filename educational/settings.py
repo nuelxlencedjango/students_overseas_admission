a9b88e3ca38216cfe38bdb2f5be9514bb9cae7bd
nuelxelence <nuelxelence@gmail.com>
2022-12-08 17:48:44 +0100

    hosting config

--- educational/settings.py
+++ educational/settings.py
@@ -47,6 +47,10 @@ ALLOWED_HOSTS = ['127.0.0.1','web-production-cbed.up.railway.app']
 
 # Application definition
 
+
+  
+
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -165,7 +169,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR ,'static') ]
 STATIC_ROOT =os.path.join(BASE_DIR ,'assets')  
 
 MEDIA_URL ='/media/'
-MEDIA_ROOT =os.path.join(BASE_DIR ,'media')
+MEDIA_ROOT =os.path.join(BASE_DIR ,'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
